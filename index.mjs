267b9aa50d69b8fbd2136a8e534c916ea41f7e56
pandi1813 <andrea.peter1813@gmail.com>
2023-02-17 18:46:14 +0000

    created prompts for rest of the input type questions and linked the answers to the readme text string

--- index.mjs
+++ index.mjs
@@ -1,19 +1,54 @@
 import inquirer from 'inquirer';
 
-let response = await inquirer
+let {title, description, installation, usage, contribution, tests} = await inquirer
   .prompt([
     /* Pass your questions in here */
     {
         type: 'input',
-        name: 'first_name',
-        message: "What's your first name",
+        name: 'title',
+        message: "What's your project title?",
       },
       {
         type: 'input',
-        name: 'last_name',
-        message: "What's your last name",
-        default() {
-          return 'Doe';
-        },
+        name: 'description',
+        message: "How would you describe the project?",
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: "How do you install the project?",
+      }, {
+        type: 'input',
+        name: 'usage',
+        message: "How is your project meant to be used?",
+      },
+      {
+        type: 'input',
+        name: 'contribution',
+        message: "How can someone contribute to teh project?",
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: "What tests have been run?",
       },
   ])
+
+  console.log(title, description)
+
+`# ${title}
+
+
+## Description
+${description}
+
+## Installation
+${installation}
+
+## Contribution 
+${contribution}
+
+## Tests
+${tests}
+`
+  
